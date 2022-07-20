bccccfd171053400f0d4b0d422c3f662ffcdca16
name <30905073+0ndrec@users.noreply.github.com>
2022-07-20 20:32:36 +0300

    Add files via upload

---
+++ Replace_a_substring_using_Python.py
@@ -0,0 +1,18 @@
+import sys
+import subprocess
+
+# Get the system argument with the path to the file
+# This file contains the pathes to the files to be compared
+targetlist = sys.argv[1]
+
+#Open the file and read the lines
+with open(targetlist, 'r') as f:
+    for line in f.readlines():
+        oldstring = line.strip()
+        newstring = oldstring.replace("target_string", "replacement_string")
+
+    #Call subprocess to rename the file
+    subprocess.run("mv " + "~" + oldstring + " " + "~" + newstring, shell=True)
+
+#Close the file
+f.close()
