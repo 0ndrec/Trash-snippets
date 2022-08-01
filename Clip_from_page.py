7b453d5fdbd5efa1a90285be14b7a671951a59e8
name <30905073+0ndrec@users.noreply.github.com>
2022-08-01 22:05:10 +0300

    Add files via upload

---
+++ Clip_from_page.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from selenium import webdriver
+import os
+import time
+import argparse
+import moviepy.editor as mpy
+
+target_url = argparse.add_argument("--url", help="Target URL", type=str)
+framerate = argparse.add_argument(
+    "--framerate", help="Framerate of the output video", type=int, default=24)
+count_of_frames = argparse.add_argument(
+    "--count", help="Count of frames to capture", type=int, default=48)
+screenshot_pause = argparse.add_argument(
+    "--pause", help="Frequency of screenshots", type=float, default=0.1)
+
+
+# Create a new instance of the Firefox driver
+gecko_options = webdriver.FirefoxOptions()
+gecko_options.add_argument("--headless")
+
+# New session
+browser = webdriver.Firefox(options=gecko_options)
+browser.get(target_url)
+
+# Create a folder for temporary files
+if not os.path.exists("TEMP"):
+    os.makedirs("TEMP")
+
+# Loop for the number of frames
+for i in range(0, count_of_frames):
+    time.sleep(screenshot_pause)
+    browser.save_screenshot("TEMP/image" + str(i) + ".png")
+
+# Open a files and combine them into a video
+images_list = []
+for frame in range(0, count_of_frames):
+    images_list.append("TEMP/image" + str(frame) + ".png")
+clip = mpy.ImageSequenceClip(images_list, fps=framerate)
+clip.write_videofile("video.mp4")
+
+# Clear temporary files
+for file in os.listdir("TEMP"):
+    os.remove("TEMP/" + file)
